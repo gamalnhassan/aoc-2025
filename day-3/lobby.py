c537acde6d73ebac4df39529955c1116ccd53b9a
Gamal Hassan <gamalnasirhassan@gmail.com>
2025-12-03 16:23:03 +0100

    Day 3 - add timing, remove strings

--- day-3/lobby.py
+++ day-3/lobby.py
@@ -1,21 +1,25 @@
 import fileinput
+import time
+
+ts = time.time()
 
 
-def rec(bank: list[int], rem: int) -> str:
+def rec(bank: list[int], rem: int, acc: int = 0) -> int:
     if rem - 1 == 0:
-        return str(max(bank))
+        return acc * 10 + max(bank)
     d = max(bank[: -(rem - 1)])
     idx = bank.index(d)
-    return str(d) + rec(bank[idx + 1 :], rem - 1)
+    return rec(bank[idx + 1 :], rem - 1, (acc * 10) + d)
 
 
 counter = 0
 rcounter = 0
 for line in fileinput.input():
     bank = [int(c) for c in line if c.isdigit()]
-    counter += int(rec(bank, 2))
-    rcounter += int(rec(bank, 12))
+    counter += rec(bank, 2)
+    rcounter += rec(bank, 12)
 
 
 print(f"Part 1: {counter}")
 print(f"Part 2: {rcounter}")
+print(f"Time taken: {time.time() - ts}")
