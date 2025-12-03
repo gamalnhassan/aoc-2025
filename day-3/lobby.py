5c591fd5be8f1da2fcf02d9eeb04177dc6c5626a
Gamal Hassan <gamalnasirhassan@gmail.com>
2025-12-03 08:18:10 +0100

    Day 3 - cleaned

--- day-3/lobby.py
+++ day-3/lobby.py
@@ -13,13 +13,7 @@ counter = 0
 rcounter = 0
 for line in fileinput.input():
     bank = [int(c) for c in line if c.isdigit()]
-    first_idx = 0
-    for idx, i in enumerate(bank[1:-1]):
-        if i > bank[first_idx]:
-            first_idx = idx + 1
-    first = bank[first_idx]
-    second = max(bank[first_idx + 1 :])
-    counter += int(f"{first}{second}")
+    counter += int(rec(bank, 2))
     rcounter += int(rec(bank, 12))
 
 
